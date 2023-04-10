6723ba7c6586d29ecf57ef141630574a40400142
Clase <christiangf2908@gmail.com>
2023-04-10 13:29:36 +0200

    Metodo para ir a guardar

--- app/src/main/java/com/example/aaap/Archivos.java
+++ app/src/main/java/com/example/aaap/Archivos.java
@@ -3,12 +3,25 @@ package com.example.aaap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class Archivos extends AppCompatActivity {
+    private EditText et1;
+    private TextView tv1;
+    private RadioButton rb1, rb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_archivos);
+        et1 = (EditText)findViewById(R.id.et2_nombre);
+        tv1 = (TextView)findViewById(R.id.tv2);
+        rb1 = (RadioButton)findViewById(R.id.radb3);
+        rb2 = (RadioButton)findViewById(R.id.radb4);
+        String resultado = getIntent().getStringExtra("tabla");
+        tv1.setText(resultado);
     }
+
 }
--- app/src/main/java/com/example/aaap/MainActivity.java
+++ app/src/main/java/com/example/aaap/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aaap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -33,5 +34,20 @@ public class MainActivity extends AppCompatActivity {
             resultado += numero + " X " + i + " = " + calc + "\n";
         }
         tv1.setText(resultado);
+    }
+    //metodo ir a guardar
+    public void archivos (View view){
+        String numero = et1_numero.getText().toString();
+
+        int valor1 = Integer.parseInt(numero);
+        int calc;
+        String resultado = "";
+        for (int i = 0; i<11;i++) {
+            calc = i * valor1;
+            resultado += numero + " X " + i + " = " + calc + "\n";
+        }
+        Intent i = new Intent(this,Archivos.class);
+        i.putExtra("tabla",resultado);
+        startActivity(i);
     }
 }
